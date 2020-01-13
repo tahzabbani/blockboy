106b9edbe2142361ccb01c4364bda9eb2665a4bb
Tahseen <tarobbani@gmail.com>
2020-01-12 19:37:51 -0600

    got collison detection

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 var cvs = document.getElementById("game");
 var ctx = cvs.getContext("2d");
 
-const keyLeft = 37;
 const keyUp = 38;
-const keyRight = 39;
-const keyDown = 40;
+const space = 32;
 
 var gravity = 0.05;
 var velocity = 0;
-var lift = 3;
-
+var lift = 3.5;
 var bottomOfCanvas = 462;
-
-// amount to move on key down
-const translate = 20;
-
 var block = new Image();
+var imgCount = 1;
+var blockX = 10;
+var blockY = 100;
+var speed = 1;
+var topPipeCoordinates = [];
+var botPipeCoordinates = [];
+var pipeWidth = 30;
+var pipeLength = 500;
+var blockWidth = 50;
+var blockHeight = 50;
+var nextPipeSpawnDistance = 300;
 
 // here because image wouldn't load and it confused me for a while
 block.onload = draw;
@@ -23,25 +27,27 @@ block.onerror = function() { alert(block.src + ' failed'); }
 
 block.src = "images/Solid_black.png";
 
-var imgCount = 1;
+topPipeCoordinates[0] = {
+    x: cvs.clientWidth - 50,
+    y: getRndInteger(-500, -100)
+};
 
-var blockX = 10;
-var blockY = 100;
+botPipeCoordinates[0] = {
+    x: cvs.clientWidth - 50,
+    y: topPipeCoordinates[0].y + 700
+};
 
-var speed = 5;
 
 document.onkeydown = moveDirection;
 
 function moveDirection(e) {
-    if (e.keyCode == keyLeft) {
-        blockX -= translate;
-    }
-    if (e.keyCode == keyUp) {
+    if (e.keyCode == keyUp || e.keyCode == space) {
         velocity -= lift;
     }
-    if (e.keyCode == keyRight) {
-        blockX += translate;
-    }
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 window.onload = function() {
@@ -58,13 +64,47 @@ function update() {
     }
 }
 
-
 function draw() {
     if (--imgCount > 0) { return; }
 
     ctx.clearRect(0, 0, cvs.width, cvs.height) // clear canvas each redraw
-    console.log(cvs.clientWidth);
-    ctx.drawImage(block, blockX, blockY, 50, 50);
+    console.log(botPipeCoordinates[0].y);
+    ctx.drawImage(block, blockX, blockY, blockWidth, blockHeight);
+
+    for (var i = 0; i < topPipeCoordinates.length; i++) {
+        ctx.fillRect(topPipeCoordinates[i].x, topPipeCoordinates[i].y, pipeWidth, pipeLength);
+        ctx.fillRect(botPipeCoordinates[i].x, botPipeCoordinates[i].y, pipeWidth, pipeLength);
+        topPipeCoordinates[i].x -= speed;
+        botPipeCoordinates[i].x -= speed;
+
+        if (topPipeCoordinates[i].x == (cvs.clientWidth - nextPipeSpawnDistance)) {
+            topPipeCoordinates.push({
+                x: cvs.clientWidth,
+                y: getRndInteger(-500, -100)
+            });
+            botPipeCoordinates.push({
+                x: cvs.clientWidth,
+                y: topPipeCoordinates[i + 1].y + 700
+            });
+        }
+
+        /*
+         * collision detection (long disgusting if-statement)
+         * maybe a better way to do this but this does the job
+         */
+        if ((blockX < botPipeCoordinates[i].x && (blockX + blockWidth) >= botPipeCoordinates[i].x &&
+                (blockY + blockHeight) >= botPipeCoordinates[i].y) ||
+            (blockX >= botPipeCoordinates[i].x && (blockX < (botPipeCoordinates[i].x + pipeWidth) &&
+                (blockY + blockHeight) >= botPipeCoordinates[i].y)) ||
+            (blockX < topPipeCoordinates[i].x && (blockX + blockWidth) >= topPipeCoordinates[i].x &&
+                blockY <= (topPipeCoordinates[i].y + pipeLength)) ||
+            (blockX >= topPipeCoordinates[i].x && blockX < (topPipeCoordinates[i].x + pipeWidth) &&
+                blockY <= (topPipeCoordinates[i].y + pipeLength))
+        ) {
+            console.log("hit");
+            location.reload();
+        }
+    }
     update();
 }
 
